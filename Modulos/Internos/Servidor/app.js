a2957aece9ac33b3a00bc645a7096510e2b3f856
Liliana Herrera <122304941+llherrerap@users.noreply.github.com>
2023-10-25 20:28:57 -0500

    Primer servidor

---
+++ Modulos/Internos/Servidor/app.js
@@ -0,0 +1,26 @@
+//Importar modulos
+const http = require('http')
+const fs = require('fs')
+
+//Crear el servidor con el metodo createServer
+const server = http.createServer((req, res) => {
+    //Leer el archivo index-html
+    fs.readFile('index.html', 'utf8', (error, data) => {
+        //si ocurre algun error
+        if (error) {
+            //La respuesta del servidor va a ser una respuesta 500
+            res.writeHead(500, {'Content-Type': 'text/html'})
+            //Definimos la respuesta final con el metodo end
+            res.end('<h1>Error 500. Se presento un error en el servidor :(</h1>')
+        }
+        res.writeHead(200, {'Content-Type':'text/html'})
+        res.end(data)
+    })
+})
+
+//Se define el puerto de salida del servidor
+const puerto = 3000;
+//Iniciar el servidor
+server.listen(puerto, ()=>{
+    console.log(`El servidor se esta ejecutando por: http://localhost:${puerto}`)
+})
